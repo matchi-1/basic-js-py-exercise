e75ec4028b4e6d06dd9a4b7014b3144262bcd363
Michelle Tolentino <misheruvynx@gmail.com>
2025-03-23 21:03:56 +0800

    ans12

---
+++ michelle_tolentino_answer_12.py
@@ -0,0 +1,16 @@
+#  Write a program that accepts a sequence of words separated by whitespace as input to print the words composed of digits only.
+#  Example:
+    #  If the following words are given as input to the program:
+    #  2 cats and 3 dogs.
+#  Then, the output of the program should be:
+    #  ['2', ' 3']
+#  In the case of input data being supplied to the question, it should be assumed to be a console input.
+
+import re
+
+def extract_digit_words():
+    text = input("Enter a sentence: ")  #
+    numbers = re.findall(r'\b\d+\b', text)
+    print(numbers) 
+
+extract_digit_words()
